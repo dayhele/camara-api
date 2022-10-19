80e713ceb20be282ddb90677b41601fe8b4e72e8
dayhele <dayhelepereira@gmail.com>
2022-10-19 17:37:04 -0300

    criando loading e consumindo API da camara (nome, partido e img)

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,67 @@
+const loadingElement = document.querySelector("#loading");
+const selectElement = document.querySelector("#politicalSelect");
+const lawmakersListElement = document.querySelector("#lawmakers-list");
+
 const camaraAPI = "https://dadosabertos.camara.leg.br/api/v2";
 
 const getAllPoliticalParty = () => {
+  toggleLoading(true);
+
   return fetch(`${camaraAPI}/partidos`)
     .then((response) => response.json())
-    .then((response) => setPoliticalPartySelect(response.dados));
+    .then((response) => {
+      toggleLoading(false);
+      setPoliticalPartySelect(response.dados);
+    });
 };
 
 const setPoliticalPartySelect = (politicalPartyList) => {
-  console.log(politicalPartyList);
-  const selectElement = document.querySelector("#politicalSelect");
-
   politicalPartyList.map((party) => {
     const option = document.createElement("option");
     option.textContent = party.sigla;
     option.value = party.id;
-    option.onchange = onPoliticalSelectionChange(party.id);
 
     selectElement.appendChild(option);
   });
 };
 
+const getLawmakers = (politicalPartyId) => {
+  toggleLoading(true);
+
+  return fetch(`${camaraAPI}/partidos/${politicalPartyId}/membros`)
+    .then((response) => response.json())
+    .then((response) => {
+      toggleLoading(false);
+      renderLawmakersList(response.dados);
+    });
+};
+
+const renderLawmakersList = (laymakersList) => {
+  lawmakersListElement.innerHTML = "";
+
+  laymakersList.map((laymaker) => {
+    lawmakersListElement.innerHTML += getLawmakerCardElement(laymaker);
+  });
+};
+
 const onPoliticalSelectionChange = (politicalPartyId) => {
-  console.log(politicalPartyId);
+  getLawmakers(politicalPartyId.value);
+};
+
+const toggleLoading = (status) => {
+  const displayStatus = status ? "flex" : "none";
+
+  loadingElement.style.display = displayStatus;
+};
+
+const getLawmakerCardElement = (cardData) => {
+  return `
+    <div class="member-card">
+      <h3 class="member-name">${cardData.nome}</h3>
+      <p>${cardData.siglaPartido}<p/>
+      <img src="${cardData.urlFoto}" alt="Foto do parlamentar">
+    </div>
+  `;
 };
 
 getAllPoliticalParty();
